77ada43a8f495b4f714feab4609e4d375eb0a022
mzdu <dan.du.pub@gmail.com>
2015-03-28 07:19:37 -0800

    beta version, waiting for more detailed data.

--- src/main.py
+++ src/main.py
@@ -11,18 +11,23 @@ import logging
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         values = dict()
-        
-        
         values['testarea'] = "this is a test string"       
-        
-        
         values['css'] = ['/static/css/main.css']
         values['javascript'] = ['']
         
         doRender(self, 'index.html', values)
+
+class AboutPageHandler(webapp2.RequestHandler):
+    def get(self):
+        values = dict()
+        values['css'] = ['/static/css/main.css']
+        values['javascript'] = ['']
+        
+        doRender(self, 'about.html', values)
         
 app = webapp2.WSGIApplication([
                                           ('/main', MainPageHandler),
+                                          ('/about', AboutPageHandler),
                                           ('/.*', MainPageHandler)
                                           ],debug = True)
     
